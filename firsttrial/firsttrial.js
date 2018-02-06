d23a18f3eaa7553dee4aaf4456690a2d13fb5bbd
ahmetkilinc <ahmetkilinc3899@gmail.com>
2018-02-06 16:20:53 +0300

    todo list added.

--- firsttrial/firsttrial.js
+++ firsttrial/firsttrial.js
@@ -21,7 +21,7 @@ var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	
 	if(page == '/'){
-	
+		
 		res.write("hey there, welcome to the page!");
 	}
 	else if(page == '/home'){
